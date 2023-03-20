3b591c636e39b116b4799824b2589b54066839d2
lordofdeceit <47636645+lordofdeceit@users.noreply.github.com>
2023-03-20 13:08:44 +0300

    files for learning repo added

---
+++ container_deployment_on_aws/getting-started-guide-project-webapp-ecs.7a4290a67e70b86b3906bb4efe02d98f47c80c50/SampleApp/start.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+echo Starting NGinx
+service nginx start
+
+echo Starting uWSGI
+cd /home/docker/code
+uwsgi -s /tmp/uwsgi.sock --manage-script-name --mount /=application:application --chown-socket=nginx:nginx
